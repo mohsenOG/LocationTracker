17462f03b257b8bccdbbd73c8ed7f33a6aec5ec9
wonderfulme <mohsen_og@yahoo.com>
2018-05-30 10:31:25 +0200

    Adding layout to fragment.

--- app/src/main/java/eu/wonderfulme/locationtracker/MainActivity.java
+++ app/src/main/java/eu/wonderfulme/locationtracker/MainActivity.java
@@ -1,5 +1,6 @@
 package eu.wonderfulme.locationtracker;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO Check if the user is online.
+
+        MainFragment mainFragment = MainFragment.newInstance();
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction().add(R.id.frameLayout_main_fragment, mainFragment).commit();
+
     }
 }
